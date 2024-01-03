101e432efbc87d9e1f2e79ba6f3a96331576d104
Ndra Center <155208771+ndracenter@users.noreply.github.com>
2024-01-03 17:01:02 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -9,10 +9,10 @@ echo ""
 echo ""
 #sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
 read -rp "Input Domain Name. Example ( kontol12 ): " -e sub
-DOMAIN=serverpremium.biz.id
-SUB_DOMAIN=${sub}.serverpremium.biz.id
-CF_ID=vscobangst111@gmail.com
-CF_KEY=4a912a2d56a1b3837d48751da5334b74a4fb8
+DOMAIN=ndrahosting.com
+SUB_DOMAIN=${sub}.ndrahosting.com
+CF_ID=cindramicin@gmail.com
+CF_KEY=cda10c3fd6b1dbf2225023c9729dfb8967af7
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
